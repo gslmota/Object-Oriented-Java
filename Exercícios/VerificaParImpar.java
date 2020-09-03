5a295ba62bc6febdad2ef33e84f0ae1de0bb7a3a
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-03 15:42:52 -0300

    Create VerificaParImpar.java

---
+++ Exercícios/VerificaParImpar.java
@@ -0,0 +1,15 @@
+//Implemente um programa em Java que leia um número inteiro e verifique se ele é par ou ímpar.
+import java.util.Scanner;
+public class VerificaParImpar {
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in);
+        int numero;
+        System.out.println("Digite um número: ");
+        numero = num.nextInt();
+        if(numero % 2 == 0){
+            System.out.println("O número " + numero + " é par!" );
+        } else {
+            System.out.println("O número " + numero + " é ímpar!" );
+        }
+    }
+}
