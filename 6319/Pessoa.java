f4862307b160c82071a0793b7a464d3c15282670
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-13 20:11:28 -0300

    Update Pessoa.java

--- 6319/Pessoa.java
+++ 6319/Pessoa.java
@@ -1,4 +1,11 @@
-package 631
+// Gabriel Sávio de Lima Mota
+package 6319;
 public class Pessoa {
+    /*Implemente uma classe Java para armazenar e imprimir a data de nascimento de
+    uma pessoa. Na sequência, crie duas instâncias dessa classe para validar as
+    operações de armazenar e imprimir.*/
+    int
+
+
     
 }
