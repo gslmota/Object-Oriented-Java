25bc73a3f5fb7d69da2c22e342aa19ef3e02f314
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-13 21:21:01 -0300

    up

--- oo/Matematica.java
+++ oo/Matematica.java
@@ -3,8 +3,19 @@ package oo;
 public class Matematica {
     int n1, n2;
     public int soma(n1, n2){
-        return n1 + n2;
+        int soma = this.n1 + this.n2;
+        return " A soma é: " + soma;
     }
 
-    public 
+    public int verificaImpar(n1){
+        if(n % 2 == 0){
+            return false;
+        } else{
+            return true;
+        }
+    }
+
+    public int calculaQuadrado(n1) {
+        return n1 * n1;
+    }
 }
---
+++ oo/MatematicaPrincipal.java
@@ -0,0 +1,17 @@
+package oo;
+
+import java.util.Scanner;
+
+public class MatematicaPrincipal {
+    public static void main(String[] args) {
+        Matematica numero1 = new Matematica();
+        Matematica numero2 = new Matematica();
+        Matematica numero3 = new Matematica();
+        Scanner input = new Scanner(System.in);
+        // Validar a Soma
+        System.out.println("Digite 2 numeros: ");
+        numero1.n1 = input.nextInt();
+        numero1.n2 = input.nextInt();
+        numero1.soma();
+    }
+}
