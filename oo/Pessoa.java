855e3a50bb40467585bdb5fee6e0c287c6e946e9
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-13 21:08:06 -0300

    up

--- oo/Pessoa.java
+++ oo/Pessoa.java
@@ -1,16 +1,13 @@
 // Gabriel Sávio de Lima Mota
 package oo;
 public class Pessoa {
-    /*Implemente uma classe Java para armazenar e imprimir a data de nascimento de
-    uma pessoa. Na sequência, crie duas instâncias dessa classe para validar as
-    operações de armazenar e imprimir.*/
     // Atributos
     int anoNascimento;
-    int mesNascimento;
+    String mesNascimento;
     int diaNascimento;
 
     // Método para armazenar
-    public void armazenarIdade(int anoNascimento, int mesNascimento, int diaNascimento) {
+    public void armazenarIdade(int anoNascimento, String mesNascimento, int diaNascimento) {
         this.anoNascimento = anoNascimento;
         this.mesNascimento = mesNascimento;
         this.diaNascimento = diaNascimento;
--- oo/PrincipalPessoa.java
+++ oo/PrincipalPessoa.java
@@ -1,27 +1,32 @@
 //  Gabriel Sávio de Lima Mota
 package oo;
-
 import java.util.Scanner;
-
 public class PrincipalPessoa {
     public static void main(String[] args) {
-        Pessoa[] pessoa = new Pessoa[2];
-        int anoNasc;
-        int mesNasc;
-        int diaNasc;
+        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa();
         Scanner input = new Scanner(System.in);
-        for(int i = 1; i <= 2; i++){
-            System.out.println("Digite o ano de nascimento da pessoa " + i);
-            anoNasc = input.nextInt();
-            System.out.println("Digite o mês de nascimento da pessoa " + i);
-            mesNasc = input.nextInt();
-            System.out.println("Digite o dia de nascimento da pessoa " + i);
-            diaNasc = input.nextInt();
-            pessoa[i].armazenarIdade(anoNasc, mesNasc, diaNasc);
-        }
-
-        for(int i = 1; i <= 2; i++){
-            pessoa[i].imprimirIdade();
-        }
+        // Cadastro da pessoa 1
+        System.out.println("Digite o ano de nascimento da Pessoa 1: ");
+        pessoa1.anoNascimento = input.nextInt();
+        System.out.println("Digite o mês de nascimento da Pessoa 1: ");
+        pessoa1.mesNascimento = input.nextLine();
+        System.out.println("Digite o dia de nascimento da Pessoa 1: ");
+        pessoa1.diaNascimento = input.nextInt();
+        pessoa1.armazenarIdade(pessoa1.anoNascimento, pessoa1.mesNascimento, pessoa1.diaNascimento);
+        // Cadastro da pessoa 2
+        System.out.println("Digite o ano de nascimento da Pessoa 2: ");
+        pessoa2.anoNascimento = input.nextInt();
+        System.out.println("Digite o mês de nascimento da Pessoa 2: ");
+        pessoa2.mesNascimento = input.nextLine();
+        System.out.println("Digite o dia de nascimento da Pessoa 2: ");
+        pessoa2.diaNascimento = input.nextInt();
+        pessoa2.armazenarIdade(pessoa2.anoNascimento, pessoa2.mesNascimento, pessoa2.diaNascimento);
+        
+        // Imprime os dados
+        System.out.println("Dados da pessoa 1");
+        pessoa1.imprimirIdade();
+        System.out.println("Dados da pessoa 2");
+        pessoa2.imprimirIdade();
     }
 }
