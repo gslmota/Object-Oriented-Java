f30b18e871e7f62515dc0362ff30e1ffdc1a1404
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-18 22:33:22 -0300

    Update PrincipalEletronico.java

--- semana3/PrincipalEletronico.java
+++ semana3/PrincipalEletronico.java
@@ -3,14 +3,16 @@ package semana3;
 public class PrincipalEletronico {
     public static void main(String[] args) {
         // Validando os Construtores da Classe Eletrônico
-        Eletronico eletronic1 = new Eletronico('azul', 120.0, 2.3, 1))
-        Eletronico eltronic2 = new Eletronico('preto');
-        Eletronico eletronic3 = new Eletronico('branco', 3);
-        Eletronico eletronic4 = new Eletronico('roxo', 1330, 3.4);
-        Eletronico eletronic5 = new Eletronico('azul escuro', 2223);
+        Eletronico eletronic1 = new Eletronico("verde", 120.0, 2.3, 1);
+        Eletronico eletronic2 = new Eletronico("preto");
+        Eletronico eletronic3 = new Eletronico("branco", 3);
+        Eletronico eletronic4 = new Eletronico("roxo", 1330, 3.4);
+        Eletronico eletronic5 = new Eletronico("azul escuro", 2223);
 
         // Exibir as informações
+       eletronic1.imprimirInformações();
        
+
 
 
 
