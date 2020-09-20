c4ae591ee206218aff08220b2bc0accc46bc094b
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-20 14:38:28 -0300

    Update VerificaParImpar.java

--- semana1/VerificaParImpar.java
+++ semana1/VerificaParImpar.java
@@ -1,6 +1,6 @@
 // Gabriel Sávio de Lima Mota   6319
 //Implemente um programa em Java que leia um número inteiro e verifique se ele é par ou ímpar.
-package exercicios;
+package semana1;
 import java.util.Scanner;
 public class VerificaParImpar {
     public static void main(String[] args){
