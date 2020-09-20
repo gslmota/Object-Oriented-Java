7fe3b516bc229796b4dfceed8cf5fac0210d14bb
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-20 14:38:53 -0300

    Update Enem.java

--- semana1/Enem.java
+++ semana1/Enem.java
@@ -1,6 +1,6 @@
 // Gabriel Sávio de Lima Mota     6319
 //Implplemente um programa em Java que leia o nome e a nota do Enem de três alunos e imprima na tela a nota e nome do aluno com menor nota.
-package exercicios;
+package semana1;
 import java.util.Scanner;
 public class Enem {
     public static void main(String args[]){
