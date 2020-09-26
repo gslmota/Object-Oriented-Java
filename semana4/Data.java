66cc60ac04211e90a0ba4ab89aa82e774e20a2e1
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-26 20:29:39 -0300

    up

---
+++ semana4/Data.java
@@ -0,0 +1,29 @@
+package semana4;
+
+public class Data {
+    private String dia, mes , ano;
+
+    public String getDia(){
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+}
--- semana4/FuncionarioUFV.java
+++ semana4/FuncionarioUFV.java
@@ -1,6 +1,41 @@
 package semana4;
 public class FuncionarioUFV {
+    // atributos
     String nome;
     int matricula;
-    
+    Data date;
+    String cargo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public Data getDate() {
+        return date;
+    }
+
+    public void setDate(Data date) {
+        this.date = date;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
 }
