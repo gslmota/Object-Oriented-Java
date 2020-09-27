28a0b2254d5b723b17368a2f62d1e7c7930b35d7
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-26 21:54:22 -0300

    Update Principal.java

--- semana4/Principal.java
+++ semana4/Principal.java
@@ -6,6 +6,10 @@ public class Principal {
     public static void main(String[] args) {
         FolhaDePagamentoUFV [] dados = new FolhaDePagamentoUFV [3];
         Scanner input = new Scanner(System.in);
+        for(int i = 0; i < dados.length; i ++){
+            dados [i] = new FolhaDePagamentoUFV();
+            
+        }
 
     }
 }
