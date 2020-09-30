e48d35546496be5b93e18d436f6a7cc021c85063
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-30 16:22:35 -0300

    Update Carro.java

--- semana5/Carro.java
+++ semana5/Carro.java
@@ -1,5 +1,44 @@
 package semana5;
 
 public class Carro {
+    // Atributos
+    private String marca, modelo;
+    private int ano,totalKm;
     
+    // Construtor
+     
+	public Carro(String marca, String modelo, int ano, int totalKm) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.ano = ano;
+		this.totalKm = totalKm;
+	}
+
+    // get e set
+    
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getTotalKm() {
+		return totalKm;
+	}
+	public void setTotalKm(int totalKm) {
+		this.totalKm = totalKm;
+    }
 }
+
