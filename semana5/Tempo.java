52169a793c025df08af23b0ceff580d251283a49
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-09-30 15:42:36 -0300

    Update Tempo.java

--- semana5/Tempo.java
+++ semana5/Tempo.java
@@ -1,5 +1,37 @@
 package semana5;
-
 public class Tempo {
+    // Atributos
+    private int hora, minuto, segundo;
     
+    // Construtor
+    public Tempo(int hora, int minuto, int segundo) {
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+    // Get e Set
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
 }
