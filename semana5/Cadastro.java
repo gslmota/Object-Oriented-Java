5429d6d886f892860308d1a541381d9b83d9eae0
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-02 20:59:50 -0300

    Update Cadastro.java

--- semana5/Cadastro.java
+++ semana5/Cadastro.java
@@ -1,7 +1,10 @@
 package semana5;
 
+import java.util.Scanner;
+
 public class Cadastro {
     String nome;
     String email;
+    Scanner input = new Scanner(System.in);
+    nome = input.next();
     
-}
