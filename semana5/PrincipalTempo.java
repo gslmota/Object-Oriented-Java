534f59f6bc2d25dd6a7985eebd2ff249ecdbfdd2
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-04 20:51:52 -0300

    Update PrincipalTempo.java

--- semana5/PrincipalTempo.java
+++ semana5/PrincipalTempo.java
@@ -4,6 +4,14 @@ public class PrincipalTempo {
     public static void main(String[] args) {
         Tempo t1 = new Tempo(8, 23, 12);
         Tempo t2 = new Tempo(3,00);
-        Tempo t3 = new Tempo
+        Tempo t3 = new Tempo(22);
+        Tempo t4 = new Tempo();
+        Tempo t5 = new Tempo(t1);
+
+        t1.toString();
+        t2.toString();
+        t3.toString();
+        t4.toString();
+        t5.tostring()
     }
 }
