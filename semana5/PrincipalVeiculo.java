2a7d4d799d91d92b230402024d3ca7dcd803e363
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-04 20:34:38 -0300

    Create PrincipalVeiculo.java

---
+++ semana5/PrincipalVeiculo.java
@@ -0,0 +1,9 @@
+package semana5;
+public class PrincipalVeiculo {
+    Veiculo moto = new Veiculo();
+    moto.acelerar();
+    Veiculo carro = new Veiculo('carro');
+    carro.acelerar();
+    Veiculo lancha = new Veiculo();
+    lancha.acelerar('lancha');
+}
