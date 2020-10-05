9a0854d622af4e9112d0d1e3a8394866ed6776bf
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-04 21:33:45 -0300

    Update PrincipalCadastro.java

--- semana5/PrincipalCadastro.java
+++ semana5/PrincipalCadastro.java
@@ -1,6 +1,12 @@
 package semana5;
-public class PrincipalCadastro{
+
+import java.util.Scanner;
+
+public class PrincipalCadastro {
     public static void main(String[] args) {
-        
+        String nome, email;
+        Scanner input = new Scanner(System.in);
+        nome = input.next();
+        email = input.next();
     }
 }
