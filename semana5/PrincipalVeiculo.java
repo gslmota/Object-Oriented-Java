05fe81570e930b4acc66ad0a5367851b606dee5e
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-04 21:16:50 -0300

    Update PrincipalVeiculo.java

--- semana5/PrincipalVeiculo.java
+++ semana5/PrincipalVeiculo.java
@@ -1,9 +1,9 @@
 package semana5;
 public class PrincipalVeiculo {
     Veiculo moto = new Veiculo("moto", 32);
-    moto.
+    moto.acelerar("honda", "moto");
     Veiculo carro = new Veiculo("carro");
     carro.acelerar();
-    Veiculo lancha = new Veiculo();
-    lancha.acelerar('lancha');
+    Veiculo lancha = new Veiculo("lancha");
+    lancha.acelerar("lancha");
 }
