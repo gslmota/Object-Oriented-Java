466bbd4c6af3037797f7206026d0051a73d2c20d
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-05 14:59:51 -0300

    Update Cadastro.java

--- semana5/Cadastro.java
+++ semana5/Cadastro.java
@@ -2,29 +2,30 @@ package semana5;
 
 public class Cadastro {
     String email, nome;
-public Cadastro(String nome, String email){
-    this.nome = nome;
-    this.email = email;
-}
+    public Cadastro(String nome, String email){
+        this.nome = nome;
+        this.email = email;
+    }
 
-public void vefificaEmail(String email){
-    String tex = email;
-    String a, b;
-    a = "@";
-    b = ".";
-    if(tex.toUpperCase().length() > 2){
-        if(tex.indexOf(a) > 0){
-            if(tex.indexOf(b) > 0){
-                System.out.println("Email e Nome Válidos");
+    public void vefificaEmail(String email){
+        String tex = email;
+        String a, b;
+        a = "@";
+        b = ".";
+        if(tex.toUpperCase().length() > 2){
+            if(tex.indexOf(a) > 0){
+                if(tex.indexOf(b) > 0){
+                    System.out.println("Email e Nome Válidos");
+                }
+                else {
+                System.out.println("Email e Nome Inválidos");
+                }
+            } else {
+                System.out.println("Email e Nome Inválidos");
             }
-            else {
-               System.out.println("Email e Nome Inválidos");
-            }
-        } else {
+        
+        } else{
             System.out.println("Email e Nome Inválidos");
         }
-    
-    } else{
-        System.out.println("Email e Nome Inválidos");
     }
 }
