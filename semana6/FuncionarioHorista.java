faec4b58a0e767976061d75fea3a2651b7962f64
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-08 21:28:05 -0300

    Create FuncionarioHorista.java

---
+++ semana6/FuncionarioHorista.java
@@ -0,0 +1,21 @@
+package semana6;
+
+public class FuncionarioHorista extends Funcionario{
+    private int horasTrabalhadas, valorHora;
+
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public int getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(int valorHora) {
+        this.valorHora = valorHora;
+    }
+}
