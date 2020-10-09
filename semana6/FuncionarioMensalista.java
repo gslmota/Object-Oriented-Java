c392c415d7fe5dc66a5ea4c169b384d088e0922b
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-08 21:25:41 -0300

    Update FuncionarioMensalista.java

--- semana6/FuncionarioMensalista.java
+++ semana6/FuncionarioMensalista.java
@@ -1,5 +1,15 @@
 package semana6;
 
 public class FuncionarioMensalista extends Funcionario{
+    private double salarioMensal;
+
+    public double getSalarioMensal() {
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(double salarioMensal) {
+        this.salarioMensal = salarioMensal;
+    }
+
     
 }
