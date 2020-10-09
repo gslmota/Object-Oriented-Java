fd05ba2bd6a624572df5b8a8e62f8b632d96c07b
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-08 21:14:26 -0300

    Update Funcionario.java

--- semana6/Funcionario.java
+++ semana6/Funcionario.java
@@ -1,5 +1,22 @@
 // Gabriel Sávio de Lima Mota   6319
 package semana6;
 public class Funcionario {
-    
+    private String nome;
+    private int matricula;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
 }
