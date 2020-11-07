dca294e0e8cf6ee4d086c6eb01ecd035d829a0f6
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-11-07 20:46:56 -0300

    up

--- semana7/CartaoDiaDosNamorados.java
+++ semana7/CartaoDiaDosNamorados.java
@@ -1,5 +1,15 @@
 package semana7;
 
-public class CartaoDiaDosNamorados {
+public class CartaoDiaDosNamorados extends CartaoWeb {
+
+    public CartaoDiaDosNamorados(String destinatario) {
+        super(destinatario);
+    }
+
+   
+    public String retornarMensagem(String remetente) {
+        return null;
+    }
+    
     
 }
--- semana7/CartaoNatal.java
+++ semana7/CartaoNatal.java
@@ -1,4 +1,12 @@
 package semana7;
-public class CartaoNatal {
-    
+
+public class CartaoNatal extends CartaoWeb {
+
+    public CartaoNatal(String destinatario) {
+        super(destinatario);
+    }
+
+    public String retornarMensagem(String remetente) {
+        return null;
+    }
 }
