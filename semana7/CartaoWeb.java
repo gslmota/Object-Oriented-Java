18fe3202da0415dbcc0d7941935561e36f3d5ad4
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-11-07 20:31:57 -0300

    Update CartaoWeb.java

--- semana7/CartaoWeb.java
+++ semana7/CartaoWeb.java
@@ -1,5 +1,19 @@
 package semana7;
 public abstract class CartaoWeb {
+
     protected String destinatario;
+
+    public CartaoWeb(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
     
+    public abstract String retornarMensagem(String remetente);
 }
