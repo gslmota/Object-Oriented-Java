a1f3f526e23c00b0e48ac501cbb55c22036f00cc
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-11-08 13:40:40 -0300

    up

--- semana7/PrincipalCartao.java
+++ semana7/PrincipalCartao.java
@@ -1,7 +1,25 @@
 package semana7;
-
+import java.util.Scanner;
 public class PrincipalCartao {
     public static void main(String[] args) {
-        CartaoWeb [] = new CartaoWeb[3];
+        String nome;
+        CartaoWeb [] cartoes = new CartaoWeb[3];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o nome do Destinario: ");
+        nome = input.next();
+        cartoes[0] = new CartaoAniversario(nome);
+        System.out.println("Digite o nome do Destinario: ");
+        nome = input.next();
+        cartoes[1] = new CartaoDiaDosNamorados(nome);
+        System.out.println("Digite o nome do Destinario: ");
+        nome = input.next();
+        cartoes[2] = new CartaoNatal(nome);
+
+        for(int i = 0; i <cartoes.length; i++){
+            String remetente;
+            System.out.println("Digite o nome do remetente da mensagem: ");
+            remetente = input.next();
+            System.out.print(cartoes[i].retornarMensagem(remetente));
+        }
     }
 }
